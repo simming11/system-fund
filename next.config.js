374cbb56a3bec32b7e7fa0a1f4d323ddf4440c8f
simming11 <abdulazem.m13@gmail.com>
2024-09-27 00:35:09 +0700

    fixed: nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['localhost', 'system-fund.vercel.app', 'system-fund']
+  },
   async headers() {
     return [
       {
