e6c1e20e0c0722117b60c6b12b79e7306ace323c
simming11 <abdulazem.m13@gmail.com>
2024-09-28 06:30:22 +0700

    fix:เวลา

--- src/app/page/scholarships/[id]/page.tsx
+++ src/app/page/scholarships/[id]/page.tsx
@@ -152,7 +152,7 @@ export default function ScholarshipDetailPage() {
     );
   }
 
-  const isApplyDisabled = scholarship ? new Date(scholarship.StartDate) > new Date(scholarship.EndDate) : true;
+  const isApplyDisabled = scholarship ? new Date(scholarship.StartDate) < new Date(scholarship.EndDate) : true;
 
   return (
     <div className="min-h-screen flex flex-col">
