01064a82f14c678cc076470707befd6d0f070c53
simming11 <abdulazem.m13@gmail.com>
2024-09-29 00:11:09 +0700

    fixed:home

--- src/app/components/headerHome/headerHome.tsx
+++ src/app/components/headerHome/headerHome.tsx
@@ -6,7 +6,7 @@ const HeaderHome = () => {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                <Image src="/images/sci.png" alt="Logo" className={styles.logo} width={100} height={100} />
+                <Image src="/images/sci.png" alt="Logo" className={styles.logo} width={150} height={100} />
                 <div className={styles.headerText}>
                     <h1>ระบบจัดการทุนการศึกษาคณะวิทยาศาสตร์และนวัตกรรมดิจิทัล มหาวิทยาลัยทักษิณ</h1>
                     <p>Student Scholarship Management System Faculty of Science and Digital Innovation, Thaksin University</p>
